97c2e97e876919115b617a37d4c8d0359b6f5ec4
Taehoon Kim <carpedm20@gmail.com>
2015-10-06 16:46:31 +0900

    edit generate.lua. more functional code

--- generate.lua
+++ generate.lua
@@ -1,14 +1,19 @@
-require 'utf8sub'
+local utf8 = require 'lua-utf8'
+local Map = require 'pl.Map'
+local Set = require 'pl.Set'
 
-Map = require 'pl.Map'
-Set = require 'pl.Set'
 
+local terms = {}
+local langs = {'en','ko'}
 
-terms = {}
-langs = {'en','ko'}
+local filename = string.format('%s_%s.txt',langs[1],langs[2])
+local print_mode = false
 
-filename = string.format('%s_%s.txt',langs[1],langs[2])
-sample_size = 100000
+if print_mode then
+  sample_size = 100
+else
+  sample_size = 100000
+end
 
 for _, lang in ipairs(langs) do
   terms[lang] = {}
@@ -18,11 +23,57 @@ terms['en']['conjunction'] = {'however','but','therefore','so','hence'}
 terms['ko']['conjunction'] = {'하지만','하지만','그러므로','그래서','그러므로'}
 terms['en']['subject'] = {'i','he','she','taehoon','janghoon'}
 terms['ko']['subject'] = {'나','그','그녀','태훈','장훈'}
-terms['en']['verb'] = {'like','liked','love','loved','hate','hated','go after','went after','catch up with','catch','catched','go to','went to','ask','asked'}
-terms['ko']['verb'] = {'좋아한다','좋아했다','사랑한다','사랑했다','싫어한다','싫어했다','따라갔다','따라갔었다','따라갔다','잡았다','잡았었다','갔다','갔었다','물어봤다','물어봤었다'}
-terms['ko']['verb2'] = {'좋아하고','좋아했고','사랑하고','사랑했고','싫어하고','싫어했고','따라갔고','따라갔었고','따라갔고','잡았고','잡았었고','갔고','갔었고','물어봤고','물어봤었고'}
+terms['en']['verb'] = {'like','love','hate','go after','catch up with','catch','go to','ask'}
+terms['ko']['verb'] = {'좋아한다','사랑한다','싫어한다','따라갔다','따라갔다','잡았다','갔다','물어봤다'}
 terms['en']['object'] = {'me','him','her','taehoon','janghoon'}
 terms['ko']['object'] = {'나','그','그녀','태훈','장훈'}
+
+function get_v_o(s, v, o, l, is_past, is_and)
+  if l == 'en' then
+    if Set{'he','she','taehoon','janghoon'}[s] and not is_past then
+      if string.sub(v,1,2) == 'go' then
+        v = string.gsub(v, 'go','goes')
+      elseif string.sub(v,1,5) == 'catch' then
+        v = string.gsub(v, 'catch','catches')
+      else
+        v = v .. 's'
+      end
+    end
+
+    if is_past then
+      if string.sub(v,1,2) == 'go' then
+        v = string.gsub(v, 'go','went')
+      elseif string.sub(v,1,5) == 'catch' then
+        v = string.gsub(v, 'catch','catched')
+      elseif string.sub(v, -1) == 'e' then
+        v = v .. 'd'
+      else
+        v = v .. 'ed'
+      end
+    end
+  elseif l == 'ko' then
+    if Set{'태훈','장훈'}[s] then s = s .. '은' else s = s .. '는' end
+    if Set{'갔다','물어봤다'}[v] then o = o .. '에게'
+    elseif Set{'태훈','장훈'}[o] then o = o .. '을' else o = o .. '를' end
+
+    if is_past then
+      if utf8.sub(v, -2) == '한다' then
+        v = utf8.sub(v, 1, utf8.len(v)-2) .. '했었다'
+      else
+        v = utf8.sub(v, 1, utf8.len(v)-1) .. '었다'
+      end
+    end
+    if is_and then
+      if utf8.sub(v, -2) == '한다' then
+        v = utf8.sub(v, 1, utf8.len(v)-2) .. '하고'
+      else
+        v = utf8.sub(v, 1, utf8.len(v)-1) .. '고'
+      end
+    end
+  end
+
+  return s, v, o
+end
 
 function mode(mode_size)
   return math.random(mode_size) % mode_size + 1
@@ -32,31 +83,12 @@ function rand(table)
   return table[math.random(#table)]
 end
 
-function make(idx,l,c,s,v,o,term,add_sentence,add_conjunct)
+function make(idx,l,c,s,v,o,add_sentence,add_conjunct,is_past)
   if l == 'en' then
-    if Set{'he','she','taehoon','janghoon'}[s] then
-      if string.sub(v,-1) ~= 'd' and string.sub(v,-5) ~= 'after' and string.sub(v,-4) ~= 'with' and string.sub(v,-2) ~= 'to' then
-        if Set{'catch'}[v] then
-          v = v .. 'es'
-        else
-          v = v .. 's'
-        end
-      elseif string.sub(v,1,2) == 'go' then
-        v = string.gsub(v, 'go','goes')
-      elseif string.sub(v,1,5) == 'catch' and string.sub(v,1,7) ~= 'catched' then
-        v = string.gsub(v, 'catch','catches')
-      end
-    end
+    s, v, o = get_v_o(s, v, o, l, is_past, add_sentence)
     str = s..' '..v..' '..o
   elseif l == 'ko' then
-    if Set{'태훈','장훈'}[s] then s = s .. '은' else s = s .. '는' end
-    if Set{'갔다','갔었다','갔고','갔었고','물어봤다','물어봤었다','물어봤고','물어봤었고'}[v] then o = o .. '에게'
-    elseif Set{'태훈','장훈'}[o] then o = o .. '을' else o = o .. '를' end
-
-    if add_sentence and idx == 1 then
-      v = term.verb2[vi]
-    end
-
+    s, v, o = get_v_o(s, v, o, l, is_past, add_sentence)
     str = s..' '..o..' '..v
   end
 
@@ -74,7 +106,9 @@ function make(idx,l,c,s,v,o,term,add_sentence,add_conjunct)
 end
 
 file = io.open (filename,'w+')
-io.output(file)
+if not print_mode then
+  io.output(file)
+end
 
 for i=1, sample_size do
   add_conjunct = math.random() > 0.5
@@ -84,24 +118,35 @@ for i=1, sample_size do
   si = math.random(#terms[langs[1]]['subject'])
   vi = math.random(#terms[langs[1]]['verb'])
   oi = math.random(#terms[langs[1]]['object'])
+  is_past = math.random() > 0.5
+
+  while si == oi do oi = math.random(#terms[langs[1]]['object']) end
+
   if add_sentence then
     ci2 = math.random(#terms[langs[1]]['conjunction'])
     si2 = math.random(#terms[langs[1]]['subject'])
     vi2 = math.random(#terms[langs[1]]['verb'])
     oi2 = math.random(#terms[langs[1]]['object'])
+    is_past2 = math.random() > 0.5
+
+    while si2 == oi2 do oi2 = math.random(#terms[langs[1]]['object']) end
   end
 
   for _, l in ipairs(langs) do
     term = terms[l]
     c, s, v, o =  term.conjunction[ci], term.subject[si], term.verb[vi], term.object[oi]
-    str = make(1,l,c,s,v,o,term,add_sentence, add_conjunct)
+    str = make(1,l,c,s,v,o,add_sentence,add_conjunct,is_past)
 
     if add_sentence then
       s, v, o =  term.subject[si2], term.verb[vi2], term.object[oi2]
-      str = str .. make(2,l,c,s,v,o,term)
+      str = str .. make(2,l,c,s,v,o,false,false,is_past2)
     end
 
-    io.write(str..'\n')
+    if print_mode then
+      print(str)
+    else
+      io.write(str..'\n')
+    end
   end
 end
 io.close()
